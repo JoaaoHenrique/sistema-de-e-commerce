6f1e3d5115479331af80a5ecc238ff907514b3ca
JoaaoHenrique <jharagao11@gmail.com>
2026-05-29 11:26:17 -0300

    adicionando metodos e atributos em Produto

--- src/main/java/ecommerce/models/Produto.java
+++ src/main/java/ecommerce/models/Produto.java
@@ -2,4 +2,18 @@ package ecommerce.models;
 
 public class Produto {
 
+    private String nome;
+    private int preco;
+    private Categoria categoria;
+
+    public Produto(String nome, int preco, Categoria categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
 }
