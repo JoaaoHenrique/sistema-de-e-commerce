00af2f9945b07bd4aa888217eabe182893977904
JoaaoHenrique <jharagao11@gmail.com>
2026-05-29 10:54:57 -0300

    adicionando metodos e atributos em Itempedido

--- src/main/java/ecommerce/models/Itempedido.java
+++ src/main/java/ecommerce/models/Itempedido.java
@@ -2,4 +2,16 @@ package ecommerce.models;
 
 public class Itempedido {
 
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(int quantidade, Produto produto) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularSubtotal() {
+
+    }
+    
 }
